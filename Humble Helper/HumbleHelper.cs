37b1a4757fa5f53f1c0bd7e4b01f71471eeb3b81
Theo Morra <td512@td512.io>
2019-10-23 04:28:56 +1300

    First build
    This solution is quite simple, it pulls all HTTPOnly cookies from an IE/WebBrowser view. Easy, right?

---
+++ Humble Helper/HumbleHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace HumbleHelper
+{
+    public class WebHelper
+    {
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool InternetGetCookieEx(string pchURL, string pchCookieName, StringBuilder pchCookieData, ref uint pcchCookieData, int dwFlags, IntPtr lpReserved);
+        const int INTERNET_COOKIE_HTTPONLY = 0x00002000;
+
+        public static string GetGlobalCookies(string uri)
+        {
+            uint datasize = 1024;
+            StringBuilder cookieData = new StringBuilder((int)datasize);
+            if (InternetGetCookieEx(uri, null, cookieData, ref datasize, INTERNET_COOKIE_HTTPONLY, IntPtr.Zero)
+                && cookieData.Length > 0)
+            {
+                return cookieData.ToString().Replace(';', ',');
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
